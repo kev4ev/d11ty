20e6ebb53a0ca3776f74364550b4050821b92958
krice <krice@pop-os.localdomain>
2022-10-11 11:26:18 -0500

    command line config

--- cli.js
+++ cli.js
@@ -11,7 +11,7 @@ program
     .argument('[input]', 'file or directory path to markdown files for conversion', '.')
     .option('-c, --collate', 'collate multiple files from the input path into a single PDF')
     .option('-n, --name <name>', 'when collating, the name of the output PDF (defaults to "collate.pdf")')
-    .option('f, --config <path>', 'relative path to .d11ty.json file containing config')
+    .option('-f, --config <path>', 'relative path to .d11ty.json file containing config')
     .option('-x, --explicit', 'only print files to PDF that explicitly include the {% d11ty %} shortcode')
     .action(async function(input, flags, cmd){
         let { config, collate, name, output, explicit } = flags;
@@ -21,11 +21,18 @@ program
             let baseConfig = new BaseConfig();
             // if config file, apply its values
             if(config){
-                if(!config.trim().endsWith('.d11ty.json')){
-                    throw new Error('Config filename must be named ".d11ty.json"');
+                if(!config.trim().endsWith('.d11ty.js')){
+                    throw new Error('Config file must be named ".d11ty.js" and export a single object or function');
                 }
                 let absPath = path.resolve(process.cwd(), path.normalize(config));
-                Object.assign(baseConfig, require(absPath));
+                let d11tyConfig = require(absPath),
+                    configObj = await (async ()=>{
+                        if(typeof d11tyConfig === 'object') return d11tyConfig;
+                        if(typeof d11tyConfig === 'function') return await d11tyConfig();
+                    })();
+                let { pdfOptions, serverOptions } = configObj;
+                if(pdfOptions) Object.assign(baseConfig.pdfOptions, pdfOptions);
+                if(serverOptions) Object.assign(baseConfig.serverOptions, serverOptions);
             }
             const pluginConfig = new CliConfig({
                 output,
